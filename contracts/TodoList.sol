2035a65e7a2958ed4838e532d0378578666dc2b8
Grace Mugoiri <gracemugoiri@gmail.com>
2021-06-17 13:06:21 +0300

    create a Task struct and map it to tasks variable

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint taskCount = 0;
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task) public tasks;
 }
